a99cd040c85bd92afac6bf7bdbd9e8380459de43
ahn seokjoo <tjrwn0716@naver.com>
2021-01-28 21:22:05 +0900

    ViewHolder 작성

--- app/src/main/java/com/example/realnavermoviefinder/MovieGridAdapter.kt
+++ app/src/main/java/com/example/realnavermoviefinder/MovieGridAdapter.kt
@@ -9,7 +9,17 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.bumptech.glide.Glide
 
+//Viewholder 패턴
+//ViewBinding 적용
+//클릭 이벤트 처리
+
 class MovieGridAdapter : BaseAdapter() {
+    class MovieViewHolder(val movieView: View) {
+        val titleTextView = movieView.findViewById<TextView>(R.id.movie_title)
+        val imageView =
+            movieView.findViewById<ImageView>(R.id.movie_image) //액티비티 아닌 곳에서는 이런식으로 앞에 입력해주어야함.
+    }
+
     private val movieList = mutableListOf<ResultGetSearchMovies.Items>()//비어있는 리스트로 일단 초기화
 
     fun submitList(data: List<ResultGetSearchMovies.Items>) {
@@ -19,26 +29,31 @@ class MovieGridAdapter : BaseAdapter() {
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val viewHolder: MovieViewHolder
+        if (convertView == null) {
 //      val inflater = parent!!.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val inflater = LayoutInflater.from(parent!!.context) //요샌 이렇게 인플레이터를 받는다.
-        //LayoutInflater = layout 리소스(xml)를 인스턴스화 할 때 필요한 객체 (인플레이터 통해서 리소스를 코드로 바꿈)
-        val movieView: View = inflater.inflate(R.layout.item_movie_iamge, parent, false)
-        //UI 표시
-        val titleTextView = movieView.findViewById<TextView>(R.id.movie_title)
-        val imageView =
-            movieView.findViewById<ImageView>(R.id.movie_image) //액티비티 아닌 곳에서는 이런식으로 앞에 입력해주어야함.
+            val inflater = LayoutInflater.from(parent!!.context) //요샌 이렇게 인플레이터를 받는다.
+            //LayoutInflater = layout 리소스(xml)를 인스턴스화 할 때 필요한 객체 (인플레이터 통해서 리소스를 코드로 바꿈)
+            val movieView: View =
+                inflater.inflate(R.layout.item_movie_image, parent, false) //인플레이트는 자원을 많이 사용함
+            //UI 표시          //findviwbyid도 비용이 비싸다  //뷰홀더는 이를 한번 쓴걸 이용해 재활용하겠다는 의미
+            viewHolder = MovieViewHolder(movieView)
+            movieView.tag = viewHolder //tag란 모든 view에 있는 딱히 용도가 정해져 있지 않은 .. 엮고싶을 때 씀
+        } else {
+            viewHolder = convertView.tag as MovieViewHolder
+        }
 
         val data = movieList[position]
-        titleTextView.text = data.title
-        Glide.with(imageView).load(data.image).into(imageView)
+        viewHolder.titleTextView.text = data.title
+        Glide.with(viewHolder.imageView).load(data.image).into(viewHolder.imageView)
 
         val result: Intent? = null
-        movieView.setOnClickListener {
-            //웹뷰에 해당 영화 link를 줌
-            result?.getStringArrayListExtra("movie")
-            // Log.d(TAG, "클릭 성공 : $binding")
-        }
-        return movieView
+//        movieView.setOnClickListener {
+//            //웹뷰에 해당 영화 link를 줌
+//            result?.getStringArrayListExtra("movie")
+//            // Log.d(TAG, "클릭 성공 : $binding")
+//        }
+        return viewHolder.movieView
     }
 
     override fun getItem(position: Int): Any {
